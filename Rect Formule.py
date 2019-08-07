dabce1c468ec7d5bc0e8e48e61d495b49c16de1a
Mochamad Aldi Taufik <52880612+alditaufik137@users.noreply.github.com>
2019-08-07 23:10:46 +0700

    Create Rect Formule.py

---
+++ Rect Formule.py
@@ -0,0 +1,26 @@
+#Luas Persegi = p*l
+
+#Keliling Persegi = 4*s
+
+print ("Menghitung Luas Dan Keliling Persegi")
+print("")
+print("")
+print ("-----_______________--.")
+print ("-----|             |--.")
+print ("-----|             |--.")
+print ("-----|             |--.")
+print ("-----|             |--.")
+print ("-----|_____________|--.")
+print ("")
+print ("")
+p = int(input("diketahui panjangnya adalah"))
+l = int(input("diketahui lebarnya adalah"))
+s = int(input("diketahui sisinya adalah"))
+print("")
+Luas = p*l
+
+Keliling = 4*s
+print("")
+
+print ("Luasnya adalah",Luas)
+print ("Kelilingnya adalah",Keliling)
